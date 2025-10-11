355e20223f391ffd946d200dfcdce60be9a8af07
JH <jiho4890@gmail.com>
2025-10-11 22:43:11 +0900

    기본 세팅

---
+++ src/test/java/com/example/shoppingmall1/ShoppingMall1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingmall1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMall1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
