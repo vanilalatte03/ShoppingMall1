a79edde60994c43a99a07fe378e14e905bfc3cea
JH <jiho4890@gmail.com>
2025-10-13 10:27:53 +0900

    상품 엔티티

--- src/main/java/com/example/shoppingmall1/entity/Item.java
+++ src/main/java/com/example/shoppingmall1/entity/Item.java
@@ -12,13 +12,13 @@ import java.time.LocalDateTime;
 @ToString
 public class Item {
 
-    private Long id; //상품코드
+    private Long id; //상품 코드
 
     private String itemNm; //상품명
 
     private int price; //가격
 
-    private int stockNumber; //재고수량
+    private int stockNumber; //재고 수량
 
     private String itemDetail; //상품 상세 설명
 
