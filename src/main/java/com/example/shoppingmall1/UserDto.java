1222941e13964e99f794d92bfdb599d0955511e2
JH <jiho4890@gmail.com>
2025-10-13 10:26:01 +0900

    테스트

---
+++ src/main/java/com/example/shoppingmall1/UserDto.java
@@ -0,0 +1,14 @@
+package com.example.shoppingmall1;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserDto {
+    private String name;
+    private Integer age;
+
+}
---
+++ src/main/java/com/example/shoppingmall1/constant/ItemSellStatus.java
@@ -0,0 +1,5 @@
+package com.example.shoppingmall1.constant;
+
+public enum ItemSellStatus {
+    SELL, SOLD_OUT
+}
