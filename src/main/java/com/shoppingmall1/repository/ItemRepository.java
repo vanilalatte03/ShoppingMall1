49a0faa617b8748efbe948c10ca18b437b0bcfb2
JH <jiho4890@gmail.com>
2025-10-28 20:47:23 +0900

    JPQL

--- src/main/java/com/shoppingmall1/repository/ItemRepository.java
+++ src/main/java/com/shoppingmall1/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.shoppingmall1.repository;
 
 import com.shoppingmall1.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,5 +16,12 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByPriceLessThan(Integer price);
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like " +
+            "%:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    @Query(value="select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailNative(@Param("itemDetail") String itemDetail);
 
 }
